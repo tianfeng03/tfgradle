c4ef63776ec8d9cc45ee2fdd2ca12799d2eb34ba
GitHub Action <action@github.com>
2023-06-08 07:47:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/tianfeng03/tfgradle/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/tianfeng03/tfgradle/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.tianfeng03.tfgradle.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
